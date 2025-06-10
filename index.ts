80ef6f7a83b82693f653b18892c5e9502e33ba56
Omar <saucyfunnel@gmail.com>
2025-06-10 12:00:26 -0500

    cambios on package

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
+import figlet from "figlet";
+
 const server = Bun.serve({
     port: 6662,
     fetch(req) {
-        return new Response("CONCHETUMAREEE");
+        const body = figlet.textSync("GODMODE");
+        return new Response(body);
     },
 });
 
-console.log(`Listening on http://localhost:${server.port}...`);
-
+console.log(`Listening on http://localhost:${server.port} ...`);
